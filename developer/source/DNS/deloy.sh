8a4a77267ba8eac35523181d4d278d772bd3bfa2
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2025-09-14 22:34:22 -0700

    a little house cleaning

--- developer/source/DNS/deloy.sh
+++
@@ -1,8 +0,0 @@
-sudo ./install_staged_tree.py
-sudo systemctl daemon-reload
-# enable nft snippet (once):
-sudo sed -i '1{/table inet filter {/!{h;s/.*/include "\/etc\/nftables.d\/30-dnsredir.nft"/;H;x}}' /etc/nftables.conf || true
-sudo nft -f /etc/nftables.conf
-
-# bring up Unbound instances (they wait for wg links):
-sudo systemctl enable --now unbound@US unbound@x6

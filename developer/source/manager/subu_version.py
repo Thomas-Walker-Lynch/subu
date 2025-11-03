8e97204fd6cec6e36ceabe4cc56514c1e6262464
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2025-11-03 05:32:37 +0000

    name updates

--- developer/source/manager/subu_version.py
+++
@@ -1,2 +0,0 @@
-# -*- mode: python; coding: utf-8; python-indent-offset: 2; indent-tabs-mode: nil -*-
-VERSION = "0.1.6"
--- developer/source/manager/test.sh
+++ developer/source/manager/test.sh
@@ -1,13 +1,13 @@
 #!/bin/env bash
 
 set -x
-./subu.py                 # -> USAGE (exit 0)
-./subu.py usage           # -> USAGE
-./subu.py -h              # -> HELP
-./subu.py --help          # -> HELP
-./subu.py help            # -> HELP
-./subu.py help WG         # -> WG topic help (or full HELP if topic unknown)
-./subu.py example         # -> EXAMPLE
-./subu.py version         # -> 0.1.4
-./subu.py -V              # -> 0.1.4
+./CLI                 # -> USAGE (exit 0)
+./CLI usage           # -> USAGE
+./CLI -h              # -> HELP
+./CLI --help          # -> HELP
+./CLI help            # -> HELP
+./CLI help WG         # -> WG topic help (or full HELP if topic unknown)
+./CLI example         # -> EXAMPLE
+./CLI version         # -> 0.1.4
+./CLI -V              # -> 0.1.4
 
